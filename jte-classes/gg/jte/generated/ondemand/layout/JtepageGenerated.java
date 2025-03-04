7867fdbcf5e6d9d62e65589d4d48448657a83364
Павел Кудря <Skelger@mail.ru>
2025-03-04 13:57:47 +0300

    add Inverted approach in JTE

--- jte-classes/gg/jte/generated/ondemand/layout/JtepageGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/layout/JtepageGenerated.java
@@ -2,12 +2,12 @@ package gg.jte.generated.ondemand.layout;
 import gg.jte.Content;
 public final class JtepageGenerated {
 	public static final String JTE_NAME = "layout/page.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,18,18,18,18,25,25,25,1,1,1,1};
+	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,21,21,21,21,30,30,30,1,1,1,1};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, Content content) {
-		jteOutput.writeContent("\n<!doctype html>\n<html lang=\"en\">\n    <head>\n        <meta charset=\"utf-8\" />\n        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n        <title>My Website</title>\n    </head>\n    <body>\n        <header>\n            <h1>My Website</h1>\n            <nav>\n                <a href=\"/\">Home</a>\n            </nav>\n        </header>\n        <main>\n            ");
+		jteOutput.writeContent("\n<!DOCTYPE html>\n<html lang=\"ru\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <title>Название вашего сайта</title>\n</head>\n<body>\n    <header>\n        <nav>\n            <h1>Courses</h1>\n            <ul>\n                <li><a href=\"/courses\">Главная</a></li>\n            </ul>\n        </nav>\n    </header>\n\n    <main>\n        ");
 		jteOutput.setContext("main", null);
 		jteOutput.writeUserContent(content);
-		jteOutput.writeContent("\n        </main>\n        <footer>\n            <p>Check out my profile on <a href=\"https://github.com/yourusername\">GitHub</a></p>\n        </footer>\n    </body>\n</html>\n");
+		jteOutput.writeContent("\n    </main>\n\n\n    <footer>\n        <p>Посетите мой профиль на <a href=\"https://github.com/Kudrya33\" target=\"_blank\">GitHub</a>.</p>\n    </footer>\n</body>\n</html>\n");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		Content content = (Content)params.get("content");
